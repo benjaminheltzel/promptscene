ca3e5d2d4524a1258a34a9932acd534a4e77a6fe
Simon Bleßmann <simon.blessmann@gmail.com>
2025-01-18 20:37:26 +0100

    Add visualization for openscene results | TODO: run mask3d on replica

--- models/Mask3D/scripts/stpls3d/stpls3d_val.sh
+++ models/Mask3D/scripts/stpls3d/stpls3d_val.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 export OMP_NUM_THREADS=3
 
+export WANDB_MODE=offline
+export WANDB_MODE=disabled
+
 CURR_DBSCAN=14.0
 CURR_TOPK=750
 CURR_QUERY=160
 CURR_SIZE=54
 
+
 # TRAIN
-python main_instance_segmentation.py \
-general.experiment_name="validation" \
-general.project_name="stpls3d" \
-data/datasets=stpls3d \
-general.num_targets=15 \
-data.num_labels=15 \
-data.voxel_size=0.333 \
-data.num_workers=10 \
-data.cache_data=true \
-data.cropping_v1=false \
-general.reps_per_epoch=100 \
-model.num_queries=${CURR_QUERY} \
-general.on_crops=true \
-model.config.backbone._target_=models.Res16UNet18B \
-data.crop_length=${CURR_SIZE} \
-general.eval_inner_core=50.0
+#python main_instance_segmentation.py \
+#general.experiment_name="validation" \
+#general.project_name="stpls3d" \
+#data/datasets=stpls3d \
+#general.num_targets=15 \
+#data.num_labels=15 \
+#data.voxel_size=0.333 \
+#data.num_workers=10 \
+#data.cache_data=true \
+#data.cropping_v1=false \
+#general.reps_per_epoch=100 \
+#model.num_queries=${CURR_QUERY} \
+#general.on_crops=true \
+#model.config.backbone._target_=models.Res16UNet18B \
+#data.crop_length=${CURR_SIZE} \
+#general.eval_inner_core=50.0
 
 # TEST
 python main_instance_segmentation.py \
@@ -40,7 +44,7 @@ model.num_queries=${CURR_QUERY} \
 general.on_crops=true \
 model.config.backbone._target_=models.Res16UNet18B \
 general.train_mode=false \
-general.checkpoint="checkpoints/stpls3d/stpls3d_val.ckpt" \
+general.checkpoint="checkpoints/stpls3d/stpls3d_benchmark_03.ckpt" \
 data.crop_length=${CURR_SIZE} \
 general.eval_inner_core=50.0 \
 general.topk_per_image=${CURR_TOPK} \
